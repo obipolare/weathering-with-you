b67c4c08fded41696823f2118aee4f0649aa6c0e
Adrian <codarko15@gmail.com>
2020-08-29 17:07:12 -0500

    ChangesOne

---
+++ components/content.js
@@ -0,0 +1,9 @@
+const articles = {
+    props: ['title', 'paragraph', 'img', 'info'],
+    template: '#first-section-template'
+}
+
+// const sectionTwo = {
+//     props: ['title', 'paragraph', 'img'],
+//     template: '#second-section-template'
+// }
