a6cf048d11d5a75b9e01604fa828ef4df6f58ada
obipolare <codarko15@gmail.com>
2021-09-05 13:51:46 -0500

    I updated a webpage from 2 years ago with vue3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,15 @@
 <script setup>
 const headerList = [
   {
-    url: "index.html",
     name: "home",
   },
   {
-    url: "blog.html",
     name: "blog",
   },
   {
-    url: "contact.html",
     name: "contact",
   },
   {
-    url: "social-media.html",
     name: "social media",
   },
 ];
@@ -21,72 +17,36 @@ const headerList = [
 
 <template>
   <header class="header">
-    <nav class="nav">
-      <ul class="main-menu">
-        <li class="main-menu__item" v-for="header in headerList" :key="header">
+    <nav class="flex justify-end">
+      <ul class="uppercase font-semibold flex">
+        <li class="px-4 pt-8 " v-for="{name} in headerList" :key="name">
           <a
-            class="main-menu__link"
-            :href="header.url"
-            v-text="header.name"
-          ></a>
+            href="/"
+          >
+          {{ name }}
+          </a>
         </li>
       </ul>
     </nav>
-    <div class="videoMediyak">
+    <section class="py-24 relative grid grid-cols-1 xl:grid-cols-2 place-items-center container mx-auto">
       <iframe
-        width="560"
-        height="315"
+        class="w-full h-96"
         src="https://www.youtube.com/embed/3Hq-hUH7BEw"
         frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
-      <h2>Just Never Give up</h2>
-    </div>
+      <h2 class="m-16 text-7xl text-center">Never Give up</h2>
+    </section>
   </header>
 </template>
 
 <style lang="scss" scoped>
 .header {
-  background-image: url("../views/assets/img/name.jpg");
+  background-image: url("../assets/img/name.jpg");
   height: 100vh;
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
-
-  .nav {
-    display: flex;
-    justify-content: end;
-  }
-
-  .main-menu {
-    display: flex;
-    margin-right: 50px;
-  }
-
-  .main-menu__item {
-    padding: 15px 15px;
-    list-style-type: none;
-  }
-
-  .main-menu__link {
-    text-decoration: none;
-    text-transform: uppercase;
-    color: white;
-    font-weight: bold;
-  }
-
-  .videoMediyak {
-    margin: 100px 100px;
-    position: relative;
-    display: flex;
-
-    h2 {
-      font-size: 50px;
-      color: white;
-      align-self: center;
-      margin: 80px;
-    }
-  }
 }
 </style>
